3816451688d2a9b383457405bf1f329b16a8f040
Maxim Vakhrushev <root@debian1.mifril.org>
2015-03-27 07:36:32 +0600

    - delete old files

--- src/timer.cpp
+++
@@ -1,9 +0,0 @@
-#include "timer.h"
-
-Timer::Timer(const std::string& _title) : title(_title), start(std::chrono::steady_clock::now())
-{}
-
-Timer::~Timer() {
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << title << std::chrono::duration_cast< std::chrono::seconds >(end - start).count() << std::endl;
-}
