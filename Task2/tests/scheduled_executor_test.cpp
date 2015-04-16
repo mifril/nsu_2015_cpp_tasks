09c3b0380bf3e0d0ca44ac73d054efd73a5e6344
Maxim Vakhrushev <mvakhrushev17@gmail.com>
2015-04-16 07:55:56 +0600

    - update tests

--- Task2/tests/scheduled_executor_test.cpp
+++ Task2/tests/scheduled_executor_test.cpp
@@ -15,7 +15,7 @@ public:
 class Periodic {
 public:
     void operator ()() {
-        std::cout << "Periodic: " << std::endl;
+        std::cout << "Periodic" << std::endl;
     }
 };
 
@@ -26,24 +26,56 @@ public:
     }
 };
 
-TEST_F(ScheduledExecutorTest, Test1) {
+TEST_F(ScheduledExecutorTest, TestSimple) {
     ScheduledExecutor ex;
-    
-	ex.Shutdown();
-    
+    ex.Shutdown();
+
     ex.ScheduleDelayedTask(Delayed());
     ex.ScheduleDelayedTask(Delayed(), std::chrono::milliseconds(1000));
 
     auto id = ex.SchedulePeriodicTask(Periodic(), std::chrono::milliseconds(0), std::chrono::milliseconds(500));
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     ex.CancelPeriodicTask(id);
-	
-	Lazy lazyTask;
+
+    Lazy lazyTask;
     auto fut = ex.ScheduleLazyTask(lazyTask);
     fut.get();
 
+    fut = ex.ScheduleLazyTask(lazyTask);
     std::cout << "HEY" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+    fut.get();
+}
+
+TEST_F(ScheduledExecutorTest, TestSimpleWithoutSleeps) {
+    ScheduledExecutor ex;
+
+    ex.ScheduleDelayedTask(Delayed());
+    ex.ScheduleDelayedTask(Delayed(), std::chrono::milliseconds(1000));
+
+    auto id = ex.SchedulePeriodicTask(Periodic(), std::chrono::milliseconds(0), std::chrono::milliseconds(500));
+    ex.CancelPeriodicTask(id);
+
+    Lazy lazyTask;
+    auto fut = ex.ScheduleLazyTask(lazyTask);
+    fut.get();
+
+    fut = ex.ScheduleLazyTask(lazyTask);
+    std::cout << "HEY" << std::endl;
+    fut.get();
+}
+
+TEST_F(ScheduledExecutorTest, TestManyTasksWithoutSleep) {
+    ScheduledExecutor ex;
+    Lazy lazyTask;
+
+    auto id = ex.SchedulePeriodicTask(Periodic(), std::chrono::milliseconds(0), std::chrono::milliseconds(1));
+    for (int i = 0; i < 1000; ++i) {
+        ex.ScheduleDelayedTask(Delayed());
+        auto fut = ex.ScheduleLazyTask(lazyTask);
+        fut.get();
+    }
+    ex.CancelPeriodicTask(id);
 }
 
 int main(int argc, char **argv) {
